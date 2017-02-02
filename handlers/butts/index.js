a65561f16fd680050f064cf5be33a0a76b97da65
Rafael <savoytruffle321@gmail.com>
2017-02-01 18:53:55 -0700

    change butts to correctly check for a setting

--- handlers/butts/index.js
+++ handlers/butts/index.js
@@ -1,8 +1,8 @@
 const axios = require('axios');
-const { requirePermission, requirePrefix } = require('../../handler');
+const { requireSetting, requirePrefix } = require('../../handler');
 const { ALLOW_NSFW_LOOKUP } = require('./constants');
 
-const butts = requirePrefix('!butts')(requirePermission(ALLOW_NSFW_LOOKUP)(
+const butts = requirePrefix('!butts')(requireSetting(ALLOW_NSFW_LOOKUP)(
   function(bot, messageInfo) {
     return axios.get('http://api.obutts.ru/butts/0/1/random')
       .then(({ data } ) => `http://media.obutts.ru/${data[0].preview}`)
